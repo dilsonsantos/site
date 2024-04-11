125d72efda263f1ad4c32eba2c7671e2c2cf48ba
Dilson Santos <dilson.santos.ti@gmail.com>
2024-04-10 22:20:21 -0300

    Primeiro commit: projeto Flask com duas páginas

--- app.py
+++ app.py
@@ -2,12 +2,14 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# Rota para a página inicial
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('/segunda-pagina')
-def segunda_pagina():
+# Rota para a segunda página
+@app.route('/second')
+def second():
     return render_template('segunda.html')
 
 if __name__ == '__main__':
